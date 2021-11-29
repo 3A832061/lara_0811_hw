71da03ce22489f7e05d6acb44e3dd6b783b9f042
yi xuan <3a832061@gm.student.ncut.edu.tw>
2021-11-30 07:57:03 +0800

    設定路由

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::get('/',[HomeController::class,'index'])->name('home');
+
+Route::get('/about',[\App\Http\Controllers\AboutController::class,'index'])->name('about');
+
+Route::get('/news',[\App\Http\Controllers\NewsController::class,'index'])->name('news');
+
